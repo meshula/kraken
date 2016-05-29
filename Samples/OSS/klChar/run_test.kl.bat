1434f5f8218969538ea8d9c4935f3348df12c856
todderasesareddot <todd.taylor@oculus.com>
2016-05-29 15:25:30 -0700

    latest kl build

--- Samples/OSS/klChar/run_test.kl.bat
+++ Samples/OSS/klChar/run_test.kl.bat
@@ -3,6 +3,21 @@ set FABRIC_LOG_LEVEL=4
 set FABRIC_PATH=Z:\dist\fabric\releases\FabricEngine-2.3.0-auto-2016052610-Windows-x86_64
 
 set KRAKEN_PATH=Z:\dist\fabric\Kraken
+:: Set the kraken path based on where this script lives
+:: We really need to have a proper environment setup eventually -TT
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+set cwd=%~dp0
+set delim=fabric\kraken
+set splitsub=@
+call set tempstring=!cwd:%delim%=%splitsub%!
+for /f "tokens=1* delims=%splitsub%" %%A in ("%tempstring%") do set part1=%%A& set part2=%%B
+set LOCAL_KRAKEN_PATH=%part1%fabric\kraken
+if not x%cwd:fabric\kraken=%==x%cwd% (
+    set KRAKEN_PATH=%LOCAL_KRAKEN_PATH%
+)
+echo KRAKEN_PATH = %KRAKEN_PATH%
+echo on
 
 set THIRD_PARTY=:\dist\fabric\ThirdParty
 
