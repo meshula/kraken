c9a9d698e6312ee0cb9e586f386b735f03e7c134
Eric Thivierge <ethivierge@gmail.com>
2016-06-30 15:42:53 -0400

    Joints now have a radius attribute and looks to the config for it's default value on creation.

--- Python/kraken/core/objects/joint.py
+++ Python/kraken/core/objects/joint.py
@@ -5,6 +5,7 @@ Joint -- Joint representation.
 
 """
 
+from kraken.core.configs.config import Config
 from kraken.core.objects.object_3d import Object3D
 
 
@@ -13,3 +14,42 @@ class Joint(Object3D):
 
     def __init__(self, name, parent=None):
         super(Joint, self).__init__(name, parent=parent)
+
+        config = Config.getInstance()
+        objSettings = config.getObjectSettings()
+        jointSettings = objSettings.get('joint', None)
+        if jointSettings is None:
+            jointSize = 1.0
+        else:
+            jointSize = jointSettings.get('size', 1.0)
+
+        self.radius = jointSize
+
+    def getRadius(self):
+        """Gets the radius of the joint.
+
+        Returns:
+            float: Radius of the joint.
+
+        """
+
+        return self.radius
+
+    def setRadius(self, radius):
+        """Sets the radius of the joint.
+
+        Args:
+            radiu (float): Radius to set the joint to.
+
+        Returns:
+            bool: True if successful.
+
+        """
+
+        assert type(radius) in (int, float), "Joint.setRadius() argument value" \
+            "is of type '" + type(radius).__name__ + "' , not of type (int, float)"
+
+        if type(radius) is int:
+            radius = float(radius)
+
+        self.radius = radius
