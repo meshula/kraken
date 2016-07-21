59a9afdc1a3633b76dc88354e6877968b202ae1c
todderasesareddot <todd.taylor@oculus.com>
2016-07-21 09:32:45 -0700

    Added support for driven blendShape attributes in Kraken.  If you add a "blendShapeName" string to a scalar attribute, it will register in the KL build as a blendShape.  I needed to change the way attributes were treated in the KL builder.  Attributes are now treated the same as objects in dependency order.

--- Python/kraken/core/objects/attributes/scalar_attribute.py
+++ Python/kraken/core/objects/attributes/scalar_attribute.py
@@ -12,11 +12,13 @@ from kraken.core.kraken_system import ks
 class ScalarAttribute(NumberAttribute):
     """Float Attribute. Implemented value type checking and limiting."""
 
-    def __init__(self, name, value=0.0, minValue=None, maxValue=None, parent=None):
+    def __init__(self, name, value=0.0, minValue=None, maxValue=None, blendShapeName='', parent=None):
         super(ScalarAttribute, self).__init__(name, value=value, minValue=minValue,
               maxValue=maxValue, parent=parent)
 
         assert type(self._value) in (int, float), "Value is not of type 'int' or 'float'."
+
+        self._blendShapeName = blendShapeName
 
 
     # ==============
@@ -51,6 +53,30 @@ class ScalarAttribute(NumberAttribute):
             return False
 
         return True
+
+
+    # ==============
+    # Value Methods
+    # ==============
+    def setBlendShapeName(self, blendShapeName):
+        """Sets the blendShape status of this attribute
+
+        Args:
+            blendShapeName (str): name of driven blend shape (seen in kl builder getShapeNames)
+
+
+        """
+        self._blendShapeName = isBlendShapeName
+
+
+    def getBlendShapeName(self):
+        """Sets the blendShape status of this attribute
+
+        Returns:
+            str: name of driven blend shape (seen in kl builder getShapeNames)
+
+        """
+        return self._blendShapeName
 
 
     def getDataType(self):
