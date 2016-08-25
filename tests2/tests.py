d3c8e3b815d523b8a1b8999b24ebd1761fb4a6cc
Eric Thivierge <ethivierge@gmail.com>
2016-08-25 15:52:32 -0400

    Added Color math object tests.

--- tests2/tests.py
+++ tests2/tests.py
@@ -3,12 +3,23 @@ import unittest
 from core import test_core
 from core import test_builder
 from core.configs import test_config
+# from core.io import test_loadSave
+from core.maths import test_color
 
 coreSuite = test_core.suite()
 builderSuite = test_builder.suite()
 configSuite = test_config.suite()
+# loadSaveSuite = test_loadSave.suite()
+loadColorSuite = test_color.suite()
 
-alltests = unittest.TestSuite([coreSuite, builderSuite, configSuite])
+suites = [
+    coreSuite,
+    builderSuite,
+    configSuite,
+    # loadSaveSuite,
+    loadColorSuite]
+
+alltests = unittest.TestSuite(suites)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
