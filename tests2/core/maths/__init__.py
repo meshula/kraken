65555a66d65cf1e15216b5480167c46de3564e22
Eric Thivierge <ethivierge@gmail.com>
2016-08-29 13:04:13 -0400

    Fixed some errors in the Mat44 library. Added Mat44 tests

--- tests2/core/maths/__init__.py
+++ tests2/core/maths/__init__.py
@@ -4,17 +4,20 @@ import unittest
 import test_color
 import test_euler
 import test_mat33
+import test_mat44
 
 loadColorSuite = test_color.suite()
 loadEulerSuite = test_euler.suite()
 loadMat33Suite = test_mat33.suite()
+loadMat44Suite = test_mat44.suite()
 
 
 def suite():
     suites = [
         loadColorSuite,
         loadEulerSuite,
-        loadMat33Suite]
+        loadMat33Suite,
+        loadMat44Suite]
 
     return unittest.TestSuite(suites)
 
