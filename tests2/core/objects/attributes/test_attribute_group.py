053e57d8e5665708753b1b7155634f950ee53e41
Eric Thivierge <ethivierge@gmail.com>
2016-08-31 21:40:17 -0400

    Attribute Group tests done.

--- tests2/core/objects/attributes/test_attribute_group.py
+++ tests2/core/objects/attributes/test_attribute_group.py
@@ -2,37 +2,106 @@
 import unittest
 
 from kraken.core.objects.attributes.attribute_group import AttributeGroup
+from kraken.core.objects.attributes.attribute import Attribute
 
 
 class TestAttributeGroup(unittest.TestCase):
 
-    def testGetName(self):
-        pass
+    # ======
+    # Setup
+    # ======
+    @classmethod
+    def setUpClass(cls):
+        cls._attributeGroup = AttributeGroup('testAttributeGroup')
 
+    def getAttributeGroup(self):
+        return self._attributeGroup
+
+    # ======
+    # Tests
+    # ======
     def testAddAttribute(self):
-        pass
-        # addAttribute
+        attributeGroup = self.getAttributeGroup()
+        attribute = Attribute('test', True)
+
+        attributeGroup.addAttribute(attribute)
+
+        self.assertTrue(attribute in attributeGroup._attributes)
+
+        attributeGroup.removeAttributeByIndex(0)
 
     def testRemoveAttributeByIndex(self):
-        pass
-        # removeAttributeByIndex
+        attributeGroup = self.getAttributeGroup()
+        attribute1 = Attribute('test1', True)
+        attribute2 = Attribute('test2', True)
+
+        attributeGroup.addAttribute(attribute1)
+        attributeGroup.addAttribute(attribute2)
+
+        removed = attributeGroup.removeAttributeByIndex(1)
+
+        self.assertTrue(removed)
+        self.assertTrue(attribute2 not in attributeGroup._attributes)
+
+        attributeGroup.removeAttributeByIndex(0)
 
     def testRemoveAttributeByName(self):
-        pass
-        # removeAttributeByName
+        attributeGroup = self.getAttributeGroup()
+        attribute1 = Attribute('test1', True)
+        attribute2 = Attribute('test2', True)
+
+        attributeGroup.addAttribute(attribute1)
+        attributeGroup.addAttribute(attribute2)
+
+        removed = attributeGroup.removeAttributeByName('test2')
+
+        self.assertTrue(removed)
+        self.assertTrue(attribute2 not in attributeGroup._attributes)
+
+        removed = attributeGroup.removeAttributeByName('test1')
 
     def testGetNumAttributes(self):
-        pass
-        # getNumAttributes
+        attributeGroup = self.getAttributeGroup()
+        attribute1 = Attribute('test1', True)
+        attribute2 = Attribute('test2', True)
+
+        attributeGroup.addAttribute(attribute1)
+        attributeGroup.addAttribute(attribute2)
+        numAttributes = attributeGroup.getNumAttributes()
+
+        self.assertEquals(numAttributes, 2)
+
+        attributeGroup.removeAttributeByName('test1')
+        attributeGroup.removeAttributeByName('test2')
+
+        self.assertEquals(attributeGroup.getNumAttributes(), 0)
 
     def testGetAttributeByIndex(self):
-        pass
-        # getAttributeByIndex
+        attributeGroup = self.getAttributeGroup()
+        attribute1 = Attribute('test1', True)
+        attribute2 = Attribute('test2', True)
+
+        attributeGroup.addAttribute(attribute1)
+        attributeGroup.addAttribute(attribute2)
+
+        attr2 = attributeGroup.getAttributeByIndex(1)
+
+        self.assertIs(attribute2, attr2)
+
+        attributeGroup.removeAttributeByName('test1')
+        attributeGroup.removeAttributeByName('test2')
 
     def testGetAttributeByName(self):
-        pass
-        # getAttributeByName
+        attributeGroup = self.getAttributeGroup()
+        attribute1 = Attribute('test1', True)
 
+        attributeGroup.addAttribute(attribute1)
+
+        attr1 = attributeGroup.getAttributeByName('test1')
+
+        self.assertIs(attribute1, attr1)
+
+        attributeGroup.removeAttributeByName('test1')
 
 
 def suite():
