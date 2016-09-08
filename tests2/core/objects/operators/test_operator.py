d003a5af06660a56f2a07cd3d62b01fdf058bcf5
Eric Thivierge <ethivierge@gmail.com>
2016-09-07 21:38:59 -0400

    Canvas and Operator tests complete.

--- tests2/core/objects/operators/test_operator.py
+++ tests2/core/objects/operators/test_operator.py
@@ -6,80 +6,112 @@ from kraken.core.objects.operators.operator import Operator
 
 class TestOperator(unittest.TestCase):
 
-    def testGetName(self):
-        pass
-
     def testGetBuildName(self):
-        pass
-        # getBuildName
+        testOp = Operator('testOp')
+        buildName = testOp.getBuildName()
+
+        self.assertEqual(buildName, 'testOpnull')
 
     def testSetFlag(self):
-        pass
-        # setFlag
+        testOp = Operator('testOp')
+        testOp.setFlag('TEST_FLAG')
+
+        self.assertTrue('TEST_FLAG' in testOp._flags)
 
     def testTestFlag(self):
-        pass
-        # testFlag
+        testOp = Operator('testOp')
+        testOp.setFlag('TEST_FLAG')
+
+        self.assertTrue(testOp.testFlag('TEST_FLAG'))
 
     def testClearFlag(self):
-        pass
-        # clearFlag
+        testOp = Operator('testOp')
+        testOp.setFlag('TEST_FLAG')
+        testOp.clearFlag('TEST_FLAG')
+
+        self.assertFalse(testOp.testFlag('TEST_FLAG'))
 
     def testGetFlags(self):
-        pass
-        # getFlags
+        testOp = Operator('testOp')
+        testOp.setFlag('TEST_FLAG')
+
+        self.assertEqual(len(testOp.getFlags()), 1)
 
     def testGetSources(self):
+        # TODO: Helge Implement this test
         pass
-        # getSources
 
     def testResizeInput(self):
-        pass
-        # resizeInput
+        testOp = Operator('testOp')
+        testOp.inputs['testInput'] = []
+
+        self.assertRaises(DeprecationWarning, lambda: testOp.resizeInput('testInput', 2))
 
     def testSetInput(self):
-        pass
-        # setInput
+        testOp = Operator('testOp')
+        testOp.inputs['testInput'] = None
+
+        self.assertRaises(NotImplementedError, lambda: testOp.setInput('testInput', 1))
 
     def testGetInput(self):
-        pass
-        # getInput
+        testOp = Operator('testOp')
+        testOp.inputs['testInput'] = None
+
+        self.assertEqual(testOp.getInput('testInput'), None)
 
     def testGetInputType(self):
-        pass
-        # getInputType
+        testOp = Operator('testOp')
+        testOp.inputs['testInput'] = None
+
+        self.assertRaises(NotImplementedError, lambda: testOp.getInputType('testInput'))
 
     def testGetInputNames(self):
-        pass
-        # getInputNames
+        testOp = Operator('testOp')
+        testOp.inputs['testInput1'] = None
+        testOp.inputs['testInput2'] = None
+
+        self.assertEqual(testOp.getInputNames(), ['testInput2', 'testInput1'])
 
     def testResizeOutput(self):
-        pass
-        # resizeOutput
+        testOp = Operator('testOp')
+        testOp.outputs['testOutput'] = []
+
+        self.assertRaises(DeprecationWarning, lambda: testOp.resizeOutput('testOutput', 2))
 
     def testSetOutput(self):
-        pass
-        # setOutput
+        testOp = Operator('testOp')
+        testOp.outputs['testOutput'] = None
+
+        self.assertRaises(NotImplementedError, lambda: testOp.setOutput('testOutput', 1))
 
     def testGetOutput(self):
-        pass
-        # getOutput
+        testOp = Operator('testOp')
+        testOp.outputs['testOutput'] = None
+
+        self.assertEqual(testOp.getOutput('testOutput'), None)
 
     def testGetOutputType(self):
-        pass
-        # getOutputType
+        testOp = Operator('testOp')
+        testOp.outputs['testOutput'] = None
+
+        self.assertRaises(NotImplementedError, lambda: testOp.getOutputType('testOutput'))
 
     def testGetOutputNames(self):
-        pass
-        # getOutputNames
+        testOp = Operator('testOp')
+        testOp.outputs['testOutput1'] = None
+        testOp.outputs['testOutput2'] = None
+
+        self.assertEqual(testOp.getOutputNames(), ['testOutput2', 'testOutput1'])
 
     def testUpdateTargets(self):
+        # TODO: Helge Implement this test
         pass
-        # updateTargets
 
     def testEvaluate(self):
-        pass
-        # evaluate
+        testOp = Operator('testOp')
+        testOp.evaluate()
+
+        self.assertTrue(testOp.testFlag('HAS_EVALUATED'))
 
 
 
