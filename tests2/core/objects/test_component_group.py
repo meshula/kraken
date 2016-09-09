8ef62ca0222aa3f22e10f9b4d3923a9080acc37f
Eric Thivierge <ethivierge@gmail.com>
2016-09-09 10:39:54 -0400

    Component Group test completed.

--- tests2/core/objects/test_component_group.py
+++ tests2/core/objects/test_component_group.py
@@ -1,21 +1,27 @@
 
 import unittest
 
+from kraken.core.objects.components.component import Component
 from kraken.core.objects.component_group import ComponentGroup
 
 
 class TestComponentGroup(unittest.TestCase):
 
     def testGetName(self):
-        pass
+        testComp = Component('testComponent', location='L')
+        testCmpGrp = ComponentGroup('testCmpGrp', testComp)
+        getName = testCmpGrp.getName()
 
-    def testGetName(self):
-        pass
-        # getName
+        self.assertTrue(type(getName is str))
+        self.assertEqual(getName, 'testComponent')
 
     def testGetNameDecoration(self):
-        pass
-        # getNameDecoration
+        testComp = Component('testComponent', location='L')
+        testCmpGrp = ComponentGroup('testCmpGrp', testComp)
+        getNameDec = testCmpGrp.getNameDecoration()
+
+        self.assertTrue(type(getNameDec is str))
+        self.assertEqual(getNameDec, ':L')
 
 
 def suite():
